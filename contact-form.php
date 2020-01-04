4f91710be004bef45d5c2fde31f0d4d96cde2917
Nyxas <moscosomaximiliano@gmail.com>
2020-01-04 15:33:44 -0300

    php from
    Test the php from

---
+++ contact-form.php
@@ -0,0 +1,19 @@
+<?php 
+	
+	if (isset($_POST['submit'])) {
+		$name = $_POST['name'];
+		$emailFrom = $_POST['email'];
+		$subject = $_POST['subject'];
+
+		$mailTo = "moscosomaximiliano@gmail.com";
+		$headers = "From: ".$emailFrom;
+		$txt = "You have received an e-mail from ".$name.".
+		\n
+		\n
+		".$message;
+
+		mail($mailTo, $subject, $txt, $headers);
+		header("Location: index.php?mailsend");
+	}
+
+ ?>

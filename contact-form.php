d194015261a81456aa9995145db6ace1b835f670
Nyxas <moscosomaximiliano@gmail.com>
2020-01-04 15:39:44 -0300

    php

--- contact-form.php
+++ contact-form.php
@@ -1,19 +1,42 @@
-<?php 
-	
-	if (isset($_POST['submit'])) {
-		$name = $_POST['name'];
-		$emailFrom = $_POST['email'];
-		$subject = $_POST['subject'];
-
-		$mailTo = "moscosomaximiliano@gmail.com";
-		$headers = "From: ".$emailFrom;
-		$txt = "You have received an e-mail from ".$name.".
-		\n
-		\n
-		".$message;
-
-		mail($mailTo, $subject, $txt, $headers);
-		header("Location: index.php?mailsend");
-	}
-
- ?>
+<?php
+session_start();
+require_once 'libs/phpmailer/PHPMailerAutoload.php';
+$errors =[];
+if(isset($_POST['name'],$_POST['email'],$_POST['subject'])){
+    $fields=[
+        'name'=>$_POST['name'],
+        'email'=>$_POST['email'],
+        'message'=>$_POST['message']
+    ];
+    foreach($fields as $field=>$data){
+        if(empty($data)){
+            $errors[]='The '.$field . ' field is required.';
+        }
+    }
+    if(empty($errors)){
+        $m=new PHPMailer;
+        $m->isSMTP();
+        $m->SMTPAuth=true;
+        $m->Host='smtp.gmail.com';
+        $m->Username='someone@gmail.com';//replace with your email address
+        $m->Password='password';//replace with your password
+        $m->SMTPSecure='ssl';
+        $m->Port=465;
+        $m->isHTML();
+        $m->Subject ='Contact form Submitted';
+        $m->Body='From:'.$fields['name'].'('.$fields['email'].')<p>'.$fields['message'].'</p>';
+        $m->FromName='Contact';
+        $m->AddAddress('someone@gmail.com','Some one');
+        if ($m->send()) {
+            header('Location:thanks.php');
+            die();
+        }else{
+            $errors[]="Sorry ,Could not send email.Try again later.";
+        }
+    }
+}else{
+    $errors[]= 'Something went wrong';
+}
+$_SESSION['errors']=$errors;
+$_SESSION['fields']=$fields;
+header ('Location:index.php');

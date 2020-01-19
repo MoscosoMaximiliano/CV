d4f4a10ea344bd04f3381143fb223b9f73b27066
Moscoso Maximiliano <31426224+MoscosoMaximiliano@users.noreply.github.com>
2020-01-19 14:29:12 -0300

    Added Programming Skill Bar

--- main.js
+++ main.js
@@ -1,33 +1,30 @@
 $(document).ready(function () {
+  $('.menu-toggler').on('click', function () {
+    $(this).toggleClass('open');
+    $('.top-nav').toggleClass('open');
+    $('html').toggleClass('locked');
+  });
 
-	$('.menu-toggler').on('click', function(){
-		$(this).toggleClass('open');
-		$('.top-nav').toggleClass('open');
-		$('html').toggleClass('locked');
-	});
+  $('.top-nav .nav-link').on('click', function () {
+    $('.menu-toggler').removeClass('open');
+    $('.top-nav').removeClass('open');
+    $('html').removeClass('locked');
+  });
 
-	$('.top-nav .nav-link').on('click', function(){
-		$('.menu-toggler').removeClass('open');
-		$('.top-nav').removeClass('open');
-		$('html').removeClass('locked');
-	});
+  $('nav a[href*="#"]').on('click', function () {
+    $('html, body').animate({
+      scrollTop: $($(this).attr('href')).offset().top - 100,
+    }, 2000);
+  });
 
-	$('nav a[href*="#"]').on('click', function () {
-		$('html, body').animate({
-			scrollTop: $($(this).attr('href')).offset().top - 100
-		}, 2000);
-	});
+  $('#up').on('click', function () {
+    $('html, body').animate({
+      scrollTop: 0,
+    }, 2000);
+  });
 
-	$('#up').on('click', function () {
-		$('html, body').animate({
-			scrollTop: 0
-		}, 2000);
-	});
-
-	AOS.init({
-		easing: 'ease',
-		duration: 800,
-	});
-
-
+  AOS.init({
+    easing: 'ease',
+    duration: 800,
+  });
 });

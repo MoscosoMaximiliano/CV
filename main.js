78ff902156269c712233d48711b06f3d78f34ba9
Moscoso Maximiliano <31426224+MoscosoMaximiliano@users.noreply.github.com>
2020-01-20 14:10:08 -0300

    nothing important

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+  //Scroll or hide the menu
   $('.menu-toggler').on('click', function () {
     $(this).toggleClass('open');
     $('.top-nav').toggleClass('open');
@@ -11,19 +12,19 @@ $(document).ready(function () {
     $('html').removeClass('locked');
   });
 
-  $('nav a[href*="#"]').on('click', function () {
+  $('nav a[href*="#"]').on('click', function () { // nav calculate to move scroll
     $('html, body').animate({
       scrollTop: $($(this).attr('href')).offset().top - 100,
     }, 2000);
   });
 
-  $('#up').on('click', function () {
+  $('#up').on('click', function () { //button for go to top of page
     $('html, body').animate({
       scrollTop: 0,
     }, 2000);
   });
 
-  AOS.init({
+  AOS.init({ //Librarie of animations
     easing: 'ease',
     duration: 800,
   });

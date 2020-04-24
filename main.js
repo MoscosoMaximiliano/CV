dddc8076598ec1aa3b422863a90a2e1b38d2e5cc
Moscoso Maximiliano <31426224+MoscosoMaximiliano@users.noreply.github.com>
2020-04-24 16:29:08 -0300

    Added multi language and fixed some images

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+var language;
+
 $(document).ready(function () {
   //Scroll or hide the menu
   $('.menu-toggler').on('click', function () {
@@ -28,4 +30,37 @@ $(document).ready(function () {
     easing: 'ease',
     duration: 800,
   });
+
+  initLanguage();
+
+  console.log('/language/' + localStorage.getItem('language') + '.json');
+  //Setting all texts with language selections
+  $('#lang-WIP').text(language.wip);
 });
+
+//Functions used for set up the language
+
+function initLanguage()
+{
+  (localStorage.getItem('language') == null) ? setLanguage('es') : setLanguage('en');
+
+  getLanguage();
+}
+
+function getLanguage() {
+
+  $.ajax({
+    url: '/language/' + localStorage.getItem('language') + '.json',
+    dataType: 'json',
+    async: false,
+    dataType: 'json',
+    success: function (lang) {
+      language = lang;
+    },
+  });
+}
+
+function setLanguage(lang) {
+  console.log(lang);
+  localStorage.setItem('language', lang);
+}
